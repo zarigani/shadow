a7b41e9829b5e7be9ddbfe7648c7733ecc57dadd
zariganitosh <XXXX@example.com>
2012-11-15 07:54:15 +0900

    Initial Commit

---
+++ shadow/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  shadow
+//
+//  Created by zarigani on 2012/11/15.
+//  Copyright (c) 2012年 bebe工房. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
